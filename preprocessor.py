98ba18fc008f79c21d605bac9a3329a8863621b3
aditya <aditya27yadav@gmail.com>
2022-08-10 01:36:15 +0200

    creating files

---
+++ preprocessor.py
@@ -0,0 +1,35 @@
+import re
+import pandas as pd
+
+
+def preprocess(data):
+    pattern = '\d{1,2}/\d{1,2}/\d{2,4},\s\d{1,2}:\d{2}\s-\s'
+    user_messages = re.split(pattern, data)[1:]
+    date = re.findall(pattern, data)
+
+    df = pd.DataFrame({'user_messages': user_messages, "date": date})
+    df['date'] = pd.to_datetime(df['date'], format='%m/%d/%y, %H:%M - ')
+
+    users = []
+    messages = []
+    for message in df['user_messages']:
+        entry = re.split('([\w\W]+?):\s', message)
+        if entry[1:]:
+            users.append(entry[1])
+            messages.append(entry[2])
+        else:
+            users.append('group_notification')
+            messages.append(entry[0])
+
+    df['user'] = users
+    df['message'] = messages
+    df.drop(columns=['user_messages'], inplace=True)
+
+    df['Year'] = df['date'].dt.year
+    df['Month'] = df['date'].dt.month_name()
+    df['Day'] = df['date'].dt.day
+    df['Hour'] = df['date'].dt.hour
+    df['Minute'] = df['date'].dt.minute
+
+    return df
+
